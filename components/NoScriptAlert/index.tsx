fefe58e0faf657906bc4ba6572a06e26d4351ba1
rotarymars <s.goto2050@gmail.com>
2026-06-13 11:05:52 +0900

    fixing overflow: hidden

--- components/NoScriptAlert/index.tsx
+++ components/NoScriptAlert/index.tsx
@@ -3,6 +3,7 @@ import styles from "./NoScriptAlert.module.css";
 export function NoScriptAlert() {
   return (
     <noscript>
+      <style>{`body { overflow: hidden; }`}</style>
       <div className={styles.overlay}>
         <div role="alert" className={styles.alert}>
           JavaScriptが無効になっています。このサイトのすべての機能を利用するには、JavaScriptを有効にしてください。
