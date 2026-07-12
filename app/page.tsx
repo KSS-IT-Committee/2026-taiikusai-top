9afe8c62c4e9748a1ff6b578c5f42314dc815219
kinoto0103 <toco0103k@gmail.com>
2026-07-12 17:14:39 +0900

    create baic design

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Image from 'next/image';
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "2026体育祭",
@@ -8,7 +10,33 @@ export const metadata: Metadata = {
 export default function Toppage() {
   return (
     <>
-      <h1>体育祭</h1>
+      <header className={styles.header}>
+        <div className={styles.themeContainer}>
+          <Image
+            src="/sports-theme.svg"
+            alt="Theme Logo"
+            width={600}
+            height={100}
+            className={styles.theme}
+            priority
+          />
+        </div>
+        <span className={styles.headerText}>
+          体育祭2026
+        </span>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.yobitai}>
+          <h1 className={`${styles.title} ${styles.titleLine} ${styles.lineBlue}`}>予備大結果</h1>
+          <div className={styles.yobitaiContent}>
+              <h2 className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}>後期</h2>
+            <div className={styles.yobitaiTopics}>
+              <h2 className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}>前期</h2 >
+            </div>
+          </div>
+
+        </div>
+      </div>
     </>
   );
 }
