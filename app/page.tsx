f4893cc9048b715fb1537083653d6f777d9c626f
kinoto0103 <toco0103k@gmail.com>
2026-07-12 18:39:47 +0900

    format the code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import Image from 'next/image';
+import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export const metadata: Metadata = {
@@ -21,26 +22,33 @@ export default function Toppage() {
             priority
           />
         </div>
-        <span className={styles.headerText}>
-          体育祭2026
-        </span>
+        <span className={styles.headerText}>体育祭2026</span>
       </header>
       <div className={styles.container}>
         <div className={styles.yobitai}>
-          <h1 className={`${styles.title} ${styles.titleLine} ${styles.lineBlue}`}>予備大結果</h1>
-          <div className={styles.content}>
-            <div className={styles.topics}>
-              <h2 className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}>後期</h2>
-              <p>ここに内容を追加</p>
-            </div>
-            <div className={styles.topics}>
-              <h2 className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}>前期</h2>
-              <p>ここに内容を追加</p>
-            </div>
+          <h1
+            className={`${styles.title} ${styles.titleLine} ${styles.lineBlue}`}
+          >
+            予備大結果
+          </h1>
+          <div className={styles.topics}>
+            <h2
+              className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}
+            >
+              後期
+            </h2>
+            <p>ここに内容を追加</p>
           </div>
-
+          <div className={styles.topics}>
+            <h2
+              className={`${styles.topicsTitle} ${styles.titleLine} ${styles.linePink}`}
+            >
+              前期
+            </h2>
+            <p>ここに内容を追加</p>
+          </div>
         </div>
-      </div>    
+      </div>
     </>
   );
 }
